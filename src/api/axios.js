47036801de56ae8f47798684e735afd74beeed14
Ahmad Mujtaba Saeed <ahmadmujtabap70@gmail.com>
2026-01-31 14:34:37 +0500

    new changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,9 +5,9 @@ import axios from 'axios';
 // const baseURL = 'https://13.53.164.183';
 // const baseURLAPI = 'https://slateblue-snake-907020.hostingersite.com/api';
 
-const baseURL = 'http://72.62.194.87:8080';
+const baseURL = 'https://cv-api.wasimdev.com';
 
-const baseURLAPI = 'http://72.62.194.87:8080/api';
+const baseURLAPI = 'https://cv-api.wasimdev.com/api';
 
 // const baseURL = 'http://127.0.0.1:8085';
 // const baseURLAPI = 'http://127.0.0.1:8085/api';
