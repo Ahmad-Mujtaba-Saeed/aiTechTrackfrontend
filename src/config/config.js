704533de96257f4850efc12b3ade81185f8980fe
Ahmad Mujtaba Saeed <ahmadmujtabap70@gmail.com>
2026-08-19 12:27:32 +0500

    v1

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
 const config = {
 
-    appUrl: 'https://cv-api.wasimdev.com',
-    apiUrl: 'https://cv-api.wasimdev.com/api',
+    // appUrl: 'https://cv-api.wasimdev.com',
+    // apiUrl: 'https://cv-api.wasimdev.com/api',
     
-// appUrl:  'http://localhost:8000',
-//  apiUrl: 'http://localhost:8000/api',
+ appUrl:  'http://localhost:8585',
+ apiUrl: 'http://localhost:8585/api',
     
     stripePublicKey:"pk_live_51SGKUzRsDMWvnk1JRPvPMqDUenPSYHKNAoCRKPiuGmcEnTLVgBicJ22OaAMtiJXlrSJA3mE9CNJOjEzWixyPvlZ000YhO6ZDRD",
 };
