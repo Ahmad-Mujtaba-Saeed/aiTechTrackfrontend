3c2e838b628357e845eee1378828a359a9722f5c
hafiz123-cell <mianamarmustafa@gmail.com>
2026-08-25 18:21:53 +0500

    Clean up config.js by removing commented URLs
    Removed commented local URLs from config.

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,6 @@ const config = {
     appUrl: 'https://cv-api.wasimdev.com',
     apiUrl: 'https://cv-api.wasimdev.com/api',
 
-    // appUrl: 'http://127.0.0.1:8000',
-    // apiUrl: 'http://127.0.0.1:8000/api',
-
     stripePublicKey: "pk_live_51SGKUzRsDMWvnk1JRPvPMqDUenPSYHKNAoCRKPiuGmcEnTLVgBicJ22OaAMtiJXlrSJA3mE9CNJOjEzWixyPvlZ000YhO6ZDRD",
 };
 
